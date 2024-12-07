4479af2afa2d8d2a44bfb24370095481c092fde1
BugHunter-X <askzenai@gmail.com>
2024-12-07 12:50:07 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 text-white p-4 rounded-lg">
+  This is some text.
+</div>
+```
+This code uses Tailwind CSS's gradient feature. However, if the `to-purple-500` class is removed or changed, it may throw an error in some browsers.
